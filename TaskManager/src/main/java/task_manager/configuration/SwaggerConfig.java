028d0ca48962f2017e22e46bfcaf647e0f51e146
timur <timur.vettest@gmail.com>
2023-12-12 21:30:41 +0300

    Настройка SwaggerConfig

--- TaskManager/src/main/java/task_manager/configuration/SwaggerConfig.java
+++ TaskManager/src/main/java/task_manager/configuration/SwaggerConfig.java
@@ -1,9 +1,28 @@
 package task_manager.configuration;
 
+import io.swagger.jaxrs.config.BeanConfig;
+
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SwaggerConfig extends Application {
 
+    public SwaggerConfig(){
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1.0.0");
+        beanConfig.setBasePath("/api");
+        beanConfig.setResourcePackage("org.jazzteam");
+        beanConfig.setScan(true);
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet<>();
+        resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+        resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+        return resources;
+    }
 
 
 }
